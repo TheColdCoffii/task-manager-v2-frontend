f8218be34a932a192e99704d09b9d320d086288a
E-V-Castillo <purelyforcode@gmail.com>
2023-10-04 08:15:20 +0800

    full wipe

--- src/context/MainProvider.jsx
+++
@@ -1,5 +0,0 @@
-import { AuthContextProvider } from './AuthContext';
-
-export default function MainProvider({ children }) {
-  return <AuthContextProvider>{children}</AuthContextProvider>;
-}
--- src/pages/Logout.jsx
+++
@@ -1,9 +0,0 @@
-import ProtectedRoute from '../components/utils/ProtectedRoute';
-
-export default function Logout() {
-  return (
-    <ProtectedRoute>
-      <div>Logout</div>;
-    </ProtectedRoute>
-  );
-}
